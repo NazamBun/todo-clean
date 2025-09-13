d8ead8e9fd11696078c5accfb43a22af8b07dc93
NazamBun <41399000+NazamBun@users.noreply.github.com>
2025-09-14 01:23:11 +0800

    feat(presentation): wire AppNavGraph in MainActivity

--- app/src/main/java/com/nazam/todo_clean/MainActivity.kt
+++ app/src/main/java/com/nazam/todo_clean/MainActivity.kt
@@ -11,37 +11,16 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.nazam.todo_clean.presentation.navigation.AppNavGraph
 import com.nazam.todo_clean.ui.theme.TodocleanTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            TodocleanTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            AppNavGraph() // ✅ Lance la navigation
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TodocleanTheme {
-        Greeting("Android")
     }
 }
